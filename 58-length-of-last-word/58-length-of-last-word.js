f4f3b637eb59350a0d792b1517b98512e8b3ac74
zaydmerzougui <60842931+zaydmerzougui@users.noreply.github.com>
2022-07-07 17:39:33 +0100

    Time: 86 ms (51.60%), Space: 41.5 MB (95.45%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.js
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -5,8 +5,6 @@
 var lengthOfLastWord = function (s) {
   let clean = s.trim().split(" ");
   let counter = 0;
-  for (let i = 0; i < clean[clean.length - 1].length; i++) {
-    counter += 1;
-  }
-  return counter
+  
+  return clean[clean.length-1].length
 };
