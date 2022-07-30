a03e57d137c0c8adf605ad244d55ad0736468deb
zaydmerzougui <60842931+zaydmerzougui@users.noreply.github.com>
2022-07-30 17:17:46 +0100

    Time: 106 ms (32.59%), Space: 43.1 MB (19.67%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.js
+++ 20-valid-parentheses/20-valid-parentheses.js
@@ -2,54 +2,22 @@
  * @param {string} s
  * @return {boolean}
  */
-var isValid = function(str) {
-
-  class Stack {
-    constructor() {
-      this.items = [];
-      this.count = 0;
+var isValid = function(s) {
+let map=new Map([
+    ['(',')'],
+    ['{' , '}'],
+    ['[' , ']']
+])
+let stack=[]
+for(let i = 0;i<s.length;i++){
+    if(map.has(s[i])){stack.push(s[i])}
+    else if(map.get(stack[stack.length-1])==s[i]) {
+        stack.pop()
+    }else{
+        return false
     }
-    // add element to top of stack
-    push(ele) {
-      this.items[this.count] = ele;
-      // console.log(this.items[this.count], this.count);
-      this.count++;
-    }
-
-    pop() {
-      if (this.count == 0) {
-        return undefined;
-      }
-      let deletedItem = this.items[this.count - 1];
-      this.items.length = this.count - 1;
-      this.count--;
-
-      return deletedItem;
-    }
-    peek() {
-      return this.items[this.items.length - 1];
-    }
-  }
-  let stack = new Stack();
-  let map = new Map([
-    ["{", "}"],
-    ["(", ")"],
-    ["[", "]"],
-  ]);
-
-  for (let i = 0; i < str.length; i++) {
-    if (map.has(str[i])) {
-      stack.push(str[i]);
-    } else if (map.get(stack.peek()) === str[i]) {
-      stack.pop();
-    } 
-      else {
-        return false;
-      }
-  }
-  
-  return !stack.items.length;
-
+}
+    return !stack.length
 
     
 };
